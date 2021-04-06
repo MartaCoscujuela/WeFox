0f4564a0dc05395aac5099163cf3d526e96b5be1
marta coscujuela <m.coscujuela.saenz@gmail.com>
2021-04-07 00:47:19 +0200

    better services and tests

--- src/app/posts/posts.service.spec.ts
+++ src/app/posts/posts.service.spec.ts
@@ -1,20 +1,17 @@
 import { of, throwError } from 'rxjs';
 import { PostsService } from './posts.service';
-import { posts as samplePosts } from './samplePosts';
+import { posts as initPosts } from './samplePosts';
 import { Post } from './post.model'
 import { HttpErrorResponse } from '@angular/common/http';
 let httpClientSpy;
 
-const BACKEND_URL = "http://localhost:3000/api/v1/posts";
-
-
 describe('PostsService', () => {
   let service: PostsService;
 
-  let posts: Post[]
+  let samplePosts: Post[]
 
   beforeEach(() => {
-    posts = [...samplePosts]
+    samplePosts = [...initPosts]
     httpClientSpy = jasmine.createSpyObj(['get', 'post', 'put', 'delete'])
     service = new PostsService(httpClientSpy as any); 
   });
@@ -23,117 +20,257 @@ describe('PostsService', () => {
     expect(service).toBeTruthy();
   });
 
-  it("should return the expected posts when getPosts is called", () => {
-    httpClientSpy.get.and.returnValue(of(posts));
+  describe('getPosts()', () => {
+
+
+    it("should call the http method get if it does not have the posts in memory when the getposts method is called",
+      () => {
+        httpClientSpy.get.and.returnValue(of(samplePosts));
     
-    service.getPosts().subscribe(fetchedPosts => {
-      expect(fetchedPosts).toEqual(posts)
-    });
-    expect(httpClientSpy.get).toHaveBeenCalledTimes(1);
-  })
-
-  it("should return the expected post when get post is called", () => {
-    const id = 1; 
-
-    httpClientSpy.get.and.returnValue(of(posts[id]));
+        service.getPosts();
     
-    service.getPost(id).subscribe(fetchedPost => {
-      expect(fetchedPost["title"]).toContain('Barcelona')
-    });
-    expect(httpClientSpy.get).toHaveBeenCalledOnceWith(BACKEND_URL+'/'+id);
-  })
-
-
-  it("should return the created post when addPost is called", () => {
-    const newPost = {
-      title: "Santiago",
-      content: "Lorem Ipsum"
-    }
-
-    const postToReturn = {
-      id: 4,
-      title: "Santiago",
-      content: "Lorem Ipsum"
-    }
-
-    httpClientSpy.post.and.returnValue(of(postToReturn));
-
-    service.addPost(newPost).subscribe();
-
-     service.addPost(newPost).subscribe(createdPost => {
-      expect(createdPost).toEqual(postToReturn)
-    }); 
-  })
+        expect(httpClientSpy.get).toHaveBeenCalledTimes(1);
+      })
   
-  it('should return an error when get post is called and the server returns an error', () => {
-    const errorResponse = new HttpErrorResponse({
-      status: 404,
-    });
+    it("should NOT call the http method get if it does have the posts in memory when the getposts method is called",
+      () => {
+        httpClientSpy.get.and.returnValue(of(samplePosts));
+        service['posts'] = samplePosts
 
-    httpClientSpy.get.and.returnValue(throwError(errorResponse));
+        service.getPosts();
+    
+        expect(httpClientSpy.get).toHaveBeenCalledTimes(0);
+      })
+  
+  
+    it("should populate the posts array when calling the http get method",
+      () => {
+        httpClientSpy.get.and.returnValue(of(samplePosts));
 
-    service.getPost(5).subscribe(
-      response => fail('expected an error, not a response'),
-      error  => expect(error.status).toBe(404)
-    );
+        service.getPosts();
+
+        expect(service['posts']).toEqual(samplePosts);
+      })
   });
 
-  it("should return the updated post when update post is called", () => {
-    const changedPost = {
-      id: 2,
-      title: "London",
-      content: "Lorem Ipsum"
-    }
+  describe('getUpdatedPosts()', () => {
 
-    httpClientSpy.put.and.returnValue(of(changedPost));
-
-    service.updatePost(changedPost, changedPost.id).subscribe(updatedPost => {
-      expect(updatedPost).toEqual(changedPost)
-    }); 
-  })
-
-  it('should return an error when update post is called and the server returns an error', () => {
-    const changedPost = {
-      id: 2,
-      title: "London",
-      content: "Lorem Ipsum"
-    }
+    it("should return the list of posts ",
+      () => {
+        service['posts'] = samplePosts;
     
-    const errorResponse = new HttpErrorResponse({
-      status: 404,
-    });
-
-    httpClientSpy.put.and.returnValue(throwError(errorResponse));
-
-    service.updatePost(changedPost, 5).subscribe(
-      response => fail('expected an error, not a response'),
-      error  => expect(error.status).toBe(404)
-    );
+        service.getUpdatedPosts().subscribe((posts) => {
+          expect(posts).toBe(samplePosts)
+        });
+        
+        service.getPosts();
+     })
   });
 
-  
-  it("should return empty when delete post is called", () => {
-    const id = 0; 
+  describe('get post', () => {
+
+    xit("should get the post from the list of posts if it is already populated",
+      () => {
+        service['posts'] = samplePosts;
     
-    httpClientSpy.delete.and.returnValue(of(''));
+        service.getPost(1).subscribe((post) => {
+          expect(post).toBe(samplePosts[0])
+        });
+        
+      })
     
-    service.deletePost(id).subscribe(response => {
-      expect(response).toBeFalsy();
-    }); 
-  })
-  
+    it("should call the http get method if the list is empty",
+      () => {
+        httpClientSpy.get.and.returnValue(of(samplePosts[0]));
     
-  it('should return an error when delete post is called and the server returns an error', () => {
-    const errorResponse = new HttpErrorResponse({
-      status: 404,
+        service.getPost(1).subscribe((post) => {
+          expect(httpClientSpy.get).toHaveBeenCalledTimes(1);
+        });
+        
+      })
+    
+        
+    it("should not call the http get method if the list is not empty",
+      () => {
+        service["posts"] = samplePosts
+
+        httpClientSpy.get.and.returnValue(of(samplePosts[0]));
+    
+        service.getPost(1).subscribe((post) => {
+          expect(httpClientSpy.get).toHaveBeenCalledTimes(0);
+        });
+        
+      })
+    
+    it('should call the error handler when the server returns an error', () => {
+      const errorResponse = new HttpErrorResponse({
+        status: 404,
+      });
+      httpClientSpy.get.and.returnValue(throwError(errorResponse));
+
+      service.getPost(1).subscribe(
+        {
+          error: (error) => {
+            expect(error).toContain('Something bad happened')
+          }
+        }
+      )
     });
-
-    httpClientSpy.delete.and.returnValue(throwError(errorResponse));
-
-    service.deletePost(5).subscribe(
-      response => fail('expected an error, not a response'),
-      error  => expect(error.status).toBe(404)
-    );
   });
 
+  describe('add post', () => {
+
+    it("should call the add post http method",
+      () => {
+        service["posts"] = samplePosts;
+        const newPost = {
+          title: "Santiago",
+          content: "Lorem Ipsum"
+        }
+
+        httpClientSpy.post.and.returnValue(of(samplePosts[0]));
+    
+        service.addPost(newPost).subscribe((post) => {
+          expect(httpClientSpy.post).toHaveBeenCalledTimes(1);
+        });
+        
+      })
+    
+    it("should add the post to the array",
+      () => {
+        service["posts"] = samplePosts;
+
+        const newPost = {
+          title: "Santiago",
+          content: "Lorem Ipsum"
+        }
+        
+        httpClientSpy.post.and.returnValue(of(samplePosts[0]));
+    
+        service.addPost(newPost).subscribe((post) => {
+          expect(service["posts"].length).toBe(4);
+        });
+      })
+    
+    
+      it('should call the error handler when the server returns an error', () => {
+        const errorResponse = new HttpErrorResponse({
+          status: 404,
+        });
+        const newPost = {
+          title: "Santiago",
+          content: "Lorem Ipsum"
+        }
+        httpClientSpy.post.and.returnValue(throwError(errorResponse));
+
+        service.addPost(newPost).subscribe(
+          {
+            error: (error) => {
+              expect(error).toContain('Something bad happened')
+            }
+          }
+        )
+      });
+    });
+
+
+
+  describe('update post', () => {
+
+    it("should call the update post http method",
+      () => {
+        service["posts"] = samplePosts;
+        const updatedPost = {
+          id: 1,
+          title: "Santiago",
+          content: "Lorem Ipsum"
+        }
+
+        httpClientSpy.put.and.returnValue(of(samplePosts[0]));
+    
+        service.updatePost(updatedPost, updatedPost.id).subscribe((post) => {
+          expect(httpClientSpy.put).toHaveBeenCalledTimes(1);
+        });
+        
+      })
+    
+    it("should update the post in the array",
+      () => {
+        service["posts"] = samplePosts;
+        const updatedPost = {
+          id: 1,
+          title: "Santiago",
+          content: "Lorem Ipsum"
+        }
+        httpClientSpy.put.and.returnValue(of(updatedPost));
+    
+        service.updatePost(updatedPost, updatedPost.id).subscribe((post) => {
+          expect(service["posts"][0].title).toEqual("Santiago");
+        });
+      })
+     
+      it('should call the error handler when the server returns an error', () => {
+        const errorResponse = new HttpErrorResponse({
+          status: 404,
+        });
+        const updatedPost = {
+          id: 1,
+          title: "Santiago",
+          content: "Lorem Ipsum"
+        }
+        httpClientSpy.put.and.returnValue(throwError(errorResponse));
+
+        service.updatePost(updatedPost, updatedPost.id).subscribe(
+          {
+            error: (error) => {
+              expect(error).toContain('Something bad happened')
+            }
+          }
+        )
+      });
+
+  });
+
+ describe('delete post', () => {
+
+    it("should call the delete post http method",
+      () => {
+        service["posts"] = samplePosts;
+        const deleteId = 1;
+
+        httpClientSpy.delete.and.returnValue(of(true));
+    
+        service.deletePost(deleteId).subscribe((post) => {
+          expect(httpClientSpy.delete).toHaveBeenCalledTimes(1);
+        });
+        
+      })
+    
+    it("should delete the post in the array",
+      () => {
+        service["posts"] = samplePosts;
+        const deleteId = 1; 
+        httpClientSpy.delete.and.returnValue(of(true));
+    
+        service.deletePost(deleteId).subscribe(() => {
+          expect(service["posts"].length).toBe(2);
+        });
+      })
+   
+       it('should call the error handler when the server returns an error', () => {
+        const errorResponse = new HttpErrorResponse({
+          status: 404,
+        });
+        const deleteId = 1; 
+        httpClientSpy.delete.and.returnValue(throwError(errorResponse));
+
+        service.deletePost(deleteId).subscribe(
+          {
+            error: (error) => {
+              expect(error).toContain('Something bad happened')
+            }
+          }
+        )
+      });
+  });
 })
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Post } from './post.model';
+import { Observable, Subject, throwError } from 'rxjs';
+import { of } from 'rxjs';
+import { catchError, map, tap } from "rxjs/operators"
 
 const BACKEND_URL = "http://localhost:3000/api/v1/posts";
 
@@ -9,29 +12,92 @@ const BACKEND_URL = "http://localhost:3000/api/v1/posts";
 })
 
 export class PostsService {
-  private posts: Post[] = [];
+  private posts: Post[];
+  private postsUpdated = new Subject<Post[]>();
 
   constructor(private http: HttpClient) { }
 
-
   getPosts() {
-    return this.http.get<Post[]>(BACKEND_URL);
+    if (this.posts)
+      return this.postsUpdated.next(this.posts);
+
+    //we only need to fetch the posts if we haven't got them in memory already
+
+    this.http.get<Post[]>(BACKEND_URL)
+      .pipe(
+        catchError(this.handleError)
+      ) 
+      .subscribe(
+      {
+        next: (fetchedPosts) => {
+          this.posts = fetchedPosts;
+          this.postsUpdated.next(this.posts);
+        }
+      }
+    );
   }
 
+  getUpdatedPosts() {
+    return this.postsUpdated as Observable<Post[]>;
+  }
+
+  // the data in the array is the same than the data in the detail. We only need to get the detail if we haven't got the post in the array already (we access the detail page directly)
   getPost(id: number) {
-    return this.http.get(BACKEND_URL+'/'+id)
+    const returnPost = this.posts?.find(post => post.id === id)
+    if (returnPost) {
+      return of(returnPost)
+    }
+    return this.http.get(BACKEND_URL + '/' + id).pipe(
+      catchError(this.handleError)
+    )
   }
 
   addPost(post: Post) {
-   return this.http.post<Post>(BACKEND_URL, post)
+    return this.http.post<Post>(BACKEND_URL, post).pipe(
+      catchError(this.handleError),
+      map(post => {
+        this.posts.push(post)
+        return post;
+      }));
   }
 
   //we could extract the id from the post argument, but since the id property is optional, this way we make sure nobody tries to call the function with a post without id
   updatePost(post: Post, postId: number) { 
-    return this.http.put<Post>(BACKEND_URL +'/'+postId, post)
+    return this.http.put<Post>(BACKEND_URL + '/' + postId, post).pipe(
+      catchError(this.handleError),
+      map(updatedPost => {
+        let postToUpdateIndex = this.posts.findIndex(post => post.id === updatedPost.id)
+        this.posts[postToUpdateIndex] = { ...updatedPost };
+        return updatedPost; 
+      })
+    )
   }
 
   deletePost(postId: number) {
-    return this.http.delete<Post>(BACKEND_URL + '/' + postId)
+    return this.http.delete<Post>(BACKEND_URL + '/' + postId).pipe(
+      catchError(this.handleError),
+      map(() => {
+        this.posts = this.posts.filter(post => post.id !== postId)
+        return true
+      })
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+
+    return throwError(
+      'Something bad happened; please try again later.');
+    
   }
 }
